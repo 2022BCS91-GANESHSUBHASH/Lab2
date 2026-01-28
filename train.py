3e13142592fbaa930ad68bda6151cfb9ce6a2df3
2022BCS91-GANESHSUBHASH <buddana22bcs91@iiitkottayam.ac.in>
2026-01-28 15:21:10 +0530

    EXP-05 Random Forest

--- train.py
+++ train.py
@@ -26,18 +26,19 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-
 # preprocessing
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
+
 # Model
 model = RandomForestRegressor(
-    n_estimators = 100,
+    n_estimators=100,
     max_depth=15,
-    random_state = 42
+    random_state=42
 )
+model.fit(X_train, y_train)
 
 # Predictions
 y_pred = model.predict(X_test)
@@ -52,6 +53,7 @@ print(f"R2 Score: {r2}")
 
 # Save model
 joblib.dump(model, "output/model/trained_model.pkl")
+joblib.dump(scaler, "output/model/scaler.pkl")
 
 # Save metrics
 metrics = {
